64040476ac47be1f5713756727b5ef1f244149d5
bugking-cyw <bugking,chenyw@foxmail.com>
2021-11-04 17:52:20 +0800

    feat: 新增登录页面

---
+++ src/pages/login/index.vue
@@ -0,0 +1,353 @@
+<template>
+	<view class="content">
+		<view class="uni-center" style="background:#efeff4; font-size:0;">
+            <image class="image" mode="widthFix" src="../../static/login-logo.png" />
+        </view>
+		<view class="input-group" >
+			<view class="input-row border">
+				<text class="title">账号：</text>
+				<m-input class="m-input" type="text" clearable focus v-model="username" placeholder="请输入账号"></m-input>
+			</view>
+			<view class="input-row border">
+				<text class="title">密码：</text>
+				<m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
+			</view>
+			
+		</view>
+		<view class="btn-row">
+			<button type="primary" class="primary" :loading="loginBtnLoading" @tap="bindLogin">登录</button>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex'
+	import mInput from '../../components/m-input.vue'
+
+
+	export default {
+		components: {
+			mInput
+		},
+		data() {
+			return {
+				username: '',
+                loginBtnLoading: false,
+				password: ''
+				
+			}
+		},
+		computed: mapState(['forcedLogin', 'hasLogin', 'univerifyErrorMsg', 'hideUniverify']),
+		onLoad() {
+			
+			
+		},
+		methods: {
+			...mapMutations(['login']),
+			
+			async loginByPwd() {
+				/**
+				 * 客户端对账号信息进行一些必要的校验。
+				 * 实际开发中，根据业务需要进行处理，这里仅做示例。
+				 */
+				if (this.username.length < 3) {
+					uni.showToast({
+						icon: 'none',
+						title: '账号最短为 3 个字符'
+					});
+					return;
+				}
+				if (this.password.length < 6) {
+					uni.showToast({
+						icon: 'none',
+						title: '密码最短为 6 个字符'
+					});
+					return;
+				}
+				const data = {
+					username: this.username,
+					password: this.password
+				};
+				this.loginBtnLoading = true
+                /* 
+				uniCloud.callFunction({
+					name: 'user-center',
+					data: {
+						action: 'login',
+						params: data
+					},
+					success: (e) => {
+						if (e.result.code == 0) {
+
+							uni.setStorageSync('uni_id_token', e.result.token)
+							uni.setStorageSync('username', e.result.username)
+							uni.setStorageSync('uni_id_has_pwd', true)
+							this.toMain(this.username);
+						} else {
+							uni.showModal({
+								content: e.result.message,
+								showCancel: false
+							})
+
+							
+						}
+					},
+					fail: (e) => {
+						uni.showModal({
+							content: JSON.stringify(e),
+							showCancel: false
+						})
+					},
+					complete: () => {
+						this.loginBtnLoading = false
+					}
+				}) */
+                this.toMain()
+			},
+			
+			bindLogin() {
+				this.loginByPwd()
+			},
+			getUserInfo({
+				detail
+			}) {
+				console.log('三方登录只演示登录api能力，暂未关联云端数据');
+				if (detail.userInfo) {
+					this.loginLocal(detail.userInfo.nickName);
+				} else {
+					uni.showToast({
+						icon: 'none',
+						title: '登陆失败'
+					});
+				}
+			},
+			loginLocal(nickName) {
+				uni.setStorageSync('username', nickName)
+				this.toMain(nickName);
+			},
+			toMain(userName) {
+				// this.login(userName);
+				/**
+				 * 强制登录时使用reLaunch方式跳转过来
+				 * 返回首页也使用reLaunch方式
+				 */
+				uni.reLaunch({
+					url: '../tabBar/home/index',
+				});
+			}
+			
+			
+		},
+		onReady() {
+			
+		}
+	}
+</script>
+
+<style>
+@import "@/components/m-icon/m-icon.css";
+
+	/*每个页面公共css */
+	page {
+		min-height: 100%;
+		display: flex;
+		font-size: 14px;
+	}
+
+	input,
+	textarea,
+	button {
+		font-size: 14px;
+	}
+
+	/* #ifdef MP-BAIDU */
+	page {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
+
+	swan-template {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
+
+	/* 原生组件模式下需要注意组件外部样式 */
+	custom-component {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
+
+	/* #endif */
+
+	/* #ifdef MP-ALIPAY */
+	page {
+		min-height: 100vh;
+	}
+
+	/* #endif */
+
+	/* 原生组件模式下需要注意组件外部样式 */
+	m-input {
+		width: 100%;
+		/* min-height: 100%; */
+		display: flex;
+		flex: 1;
+	}
+
+	.content {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		background-color: #efeff4;
+		padding: 10px;
+	}
+
+	.input-group {
+		background-color: #ffffff;
+		margin-top: 20px;
+		position: relative;
+	}
+
+	.input-group::before {
+		position: absolute;
+		right: 0;
+		top: 0;
+		left: 0;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+
+	.input-group::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+
+	.input-row {
+		display: flex;
+		flex-direction: row;
+		position: relative;
+		/* font-size: 18px; */
+		height: 40px;
+		line-height: 40px;
+	}
+
+	.input-row .title {
+		width: 70px;
+		padding-left: 15px;
+	}
+
+	.input-row.border::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 8px;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+
+	.btn-row {
+		margin-top: 25px;
+		padding: 10px;
+	}
+
+	button.primary {
+		background-color: #0faeff;
+	}
+	.login-type {
+		display: flex;
+		justify-content: center;
+	}
+
+	.login-type-btn {
+		line-height: 30px;
+		margin: 0px 15px;
+	}
+
+	.login-type-btn.act {
+		color: #0FAEFF;
+		border-bottom: solid 1px #0FAEFF;
+	}
+
+	.send-code-btn {
+		width: 120px;
+		text-align: center;
+		background-color: #0FAEFF;
+		color: #FFFFFF;
+	}
+
+	.action-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	.action-row navigator {
+		color: #007aff;
+		padding: 0 10px;
+	}
+
+	.oauth-row {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+		flex-wrap: wrap;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+	}
+
+	.oauth-image {
+		position: relative;
+		width: 50px;
+		height: 50px;
+		border: 1px solid #dddddd;
+		border-radius: 50px;
+		background-color: #ffffff;
+	}
+
+	.oauth-image image {
+		width: 30px;
+		height: 30px;
+		margin: 10px;
+	}
+
+	.oauth-image button {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+	}
+
+	.captcha-view {
+		line-height: 0;
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		position: relative;
+		background-color: #f3f3f3;
+	}
+</style>
