5d0f5e31b3ace1f6d3886f7f70c878dba2370b08
bugking-cyw <bugking,chenyw@foxmail.com>
2021-11-04 15:22:31 +0800

    build: 首次导入

---
+++ src/pages/tabBar/dir/index.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="page dataease-main">
+		<uni-list>
+                
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="1" />
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="2" />
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="3" />
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="4" />
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="3" />
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="2" />
+            <uni-list-item title="2021年全国GDP数据1"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="4" />
+            
+        </uni-list>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			showSwiper: false,
+			imgUrls: [
+				'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/b4b60b10-5168-11eb-bd01-97bc1429a9ff.jpg',
+				'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/b1dcfa70-5168-11eb-bd01-97bc1429a9ff.jpg'
+			]
+		};
+	},
+	/**
+	 * 当 searchInput 配置 disabled 为 true 时触发
+	 */
+	onNavigationBarSearchInputClicked(e) {
+		console.log('事件执行了')
+		/* uni.navigateTo({
+			url: '/pages/template/nav-search-input/detail/detail'
+		}); */
+	},
+	/**
+	 *  点击导航栏 buttons 时触发
+	 */
+	/* onNavigationBarButtonTap() {
+		uni.showModal({
+			title: '提示',
+			content: '用户点击了功能按钮，这里仅做展示。',
+			success: res => {
+				if (res.confirm) {
+					console.log('用户点击了确定');
+				}
+			}
+		});
+	} */
+};
+</script>
+
+<style>
+image,
+swiper,
+.img-view {
+	width: 750rpx;
+	width: 100%;
+	height: 500rpx;
+}
+.page-section-title {
+	margin-top: 50rpx;
+}
+
+.dataease-main {
+    
+    width: 100%;
+    position: absolute;
+    top: 40px;
+    height: calc(100vh - 80px);
+    margin: 5px 10px;
+}
+
+</style>
---
+++ src/pages/tabBar/me/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <view>
+        <view>
+            <uni-card :is-shadow="true" >
+                <view class="uni-flex uni-row">
+                    <view class="text uni-flex" style="width: 200rpx;height: 220rpx;-webkit-justify-content: center;justify-content: center;-webkit-align-items: center;align-items: center;">
+                        <image src="../../../static/logo.png" style="width: 150rpx;height: 150rpx;"></image>
+                    </view>
+                    <view class="uni-flex uni-column" style="-webkit-flex: 1;flex: 1;">
+                        <view class="text" style="height: 90rpx;text-align: left;padding-left: 20rpx;padding-top: 40rpx; front-size: 25rpx;">
+                            陈亚文
+                        </view>
+                        <view class="uni-flex uni-row" style="padding-left: 20rpx;">
+                            <view class="text" style="-webkit-flex: 1;flex: 1;">管理员</view>
+                            <view class="text" style="-webkit-flex: 1;flex: 1;">更多信息</view>
+                        </view>
+                    </view>
+                </view>
+            </uni-card>
+        </view>
+
+        <view>
+            <uni-list>
+                
+                <uni-list-item title="语言"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="简体中文" />
+                <uni-list-item title="关于"  showArrow thumb="../../../static/folder-close.png" thumb-size="base" rightText="系统信息" />
+            </uni-list>
+        </view>
+
+        <view>
+            <button type="default" @click="">注销</button>
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
